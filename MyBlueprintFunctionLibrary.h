a7220c8e0e91d82d8ebc286a2af8aec356d74da8
Cyrus Cook <11858105+cyruscook@users.noreply.github.com>
2019-02-17 17:28:40 +0000

    Added Restart App If Necessary and Run Callbacks

--- MyBlueprintFunctionLibrary.h
+++ MyBlueprintFunctionLibrary.h
@@ -14,8 +14,18 @@ class SHOOTER_API UMyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 	
+public:
+
 	// Returns whether the Steam SDK is initialized and if not starts it up
-	UFUNCTION(BlueprintPure, Category = "C++ Function Library|Online|Steam", meta = (DisplayName = "Is Steam Running", Keywords = "is steam api sdk cleint running launched"))
+	UFUNCTION(BlueprintCallable, Category = "C++ Function Library|Online|Steam", meta = (DisplayName = "Is Steam Running", Keywords = "is steam api sdk cleint running launched"))
 		static bool IsSteamAPIRunning();
+	
+	// If the game is not running through steam it will restart the game through steam
+	UFUNCTION(BlueprintCallable, Category = "C++ Function Library|Online|Steam", meta = (DisplayName = "Restart Through Steam", Keywords = "restart game through steam check running launched sdk"))
+		static void RestartAppIfNecessary(int32 AppID);
+	
+	// Run any pending asynchronous callbacks. Best to run this function on Game State's tick
+	UFUNCTION(BlueprintCallable, Category = "C++ Function Library|Online|Steam", meta = (DisplayName = "Run Callbacks", Keywords = "run check refresh tick callbacks steam sdk"))
+		static void RunCallbacks();
 	
 };
