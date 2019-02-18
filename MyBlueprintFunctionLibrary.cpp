952cc9b408c98c8dd2b6857ffddb9ec5eed8e507
Cyrus Cook <11858105+cyruscook@users.noreply.github.com>
2019-02-18 12:48:47 +0000

    Added GetAllDLCs and isAppInstalled

--- MyBlueprintFunctionLibrary.cpp
+++ MyBlueprintFunctionLibrary.cpp
@@ -9,10 +9,44 @@ bool UMyBlueprintFunctionLibrary::IsSteamAPIRunning() {
 	return SteamAPI_Init();
 }
 
-void UMyBlueprintFunctionLibrary::RestartAppIfNecessary(int32 AppID) {
-	SteamAPI_RestartAppIfNecessary(AppID);
+void UMyBlueprintFunctionLibrary::RestartAppIfNecessary(const int32 SteamAppID = 480) {
+	// Since we can only run a game through steam on a shipping build, this function should do nothing if it isn't a shipping build
+	#if UE_BUILD_SHIPPING
+	if (SteamAPI_RestartAppIfNecessary(SteamAppID)) {
+		FGenericPlatformMisc::RequestExit(true);
+	}
+	#endif
 }
 
 void UMyBlueprintFunctionLibrary::RunCallbacks() {
 	SteamAPI_RunCallbacks();
+}
+
+/* ------------------------------------------------------------ ISteamApps ------------------------------------------------------------ */
+
+bool UMyBlueprintFunctionLibrary::GetAllDLCs(TArray<FSteamDLC>& allDLCs) {
+	bool wasSuccessful = true;
+	// Get the total number of DLCs and iterate through them
+	int32 DLCCount = SteamApps()->GetDLCCount();
+	for (int i = 0; i < DLCCount; ++i) {
+		// Request DLC at index i
+		AppId_t appId;
+		bool available;
+		char name[128];
+		bool success = SteamApps()->BGetDLCDataByIndex(i, &appId, &available, name, 128);
+		// If it was successful add it to the array to be returned, otherwise make sure we tell the user it was unsuccessful
+		if (success) {
+			allDLCs[i].DLCIndex = i;
+			allDLCs[i].steamAppID = appId;
+			allDLCs[i].isAvailable = available;
+			allDLCs[i].DLCName = name;
+		} else {
+			wasSuccessful = false;
+		}
+	}
+	return wasSuccessful;
+}
+
+bool UMyBlueprintFunctionLibrary::isAppInstalled(int steamAppID) {
+	return SteamApps()->BIsAppInstalled(steamAppId);
 }
--- MyBlueprintFunctionLibrary.h
+++ MyBlueprintFunctionLibrary.h
@@ -6,9 +6,35 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "MyBlueprintFunctionLibrary.generated.h"
 
-/**
- * 
- */
+
+// Define Structs
+
+// Contains data about a given DLC
+USTRUCT(BlueprintType, Category = "Steam|Steam Apps")
+struct FSteamDLC
+{
+	GENERATED_USTRUCT_BODY();
+
+public:
+
+	// Index of the DLC between 0 and the total amount of DLCs
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Steam|Steam Apps")
+		int32 DLCIndex;
+
+	// The App ID of the DLC.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Steam|Steam Apps")
+		int32 steamAppID;
+
+	// Whether the DLC is currently available.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Steam|Steam Apps")
+		bool isAvailable;
+
+	// The name of the DLC by copying it into this buffer.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Steam|Steam Apps")
+		FString DLCName;
+};
+
+// Define Functions
 UCLASS()
 class SHOOTER_API UMyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 {
@@ -17,15 +43,28 @@ class SHOOTER_API UMyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 public:
 
 	// Returns whether the Steam SDK is initialized and if not starts it up
-	UFUNCTION(BlueprintCallable, Category = "C++ Function Library|Online|Steam", meta = (DisplayName = "Is Steam Running", Keywords = "is steam api sdk cleint running launched"))
+	UFUNCTION(BlueprintCallable, Category = "Steam", meta = (DisplayName = "Is Steam Running", Keywords = "is steam api sdk cleint running launched initialize"))
 		static bool IsSteamAPIRunning();
 	
-	// If the game is not running through steam it will restart the game through steam
-	UFUNCTION(BlueprintCallable, Category = "C++ Function Library|Online|Steam", meta = (DisplayName = "Restart Through Steam", Keywords = "restart game through steam check running launched sdk"))
-		static void RestartAppIfNecessary(int32 AppID);
+	// Checks if your executable was launched through Steam and relaunches it through Steam if it wasn't.
+	UFUNCTION(BlueprintCallable, Category = "Steam", meta = (DisplayName = "Restart Through Steam", Keywords = "restart game through steam check running launched sdk"))
+		static void RestartAppIfNecessary(const int32 SteamAppID);
 	
 	// Run any pending asynchronous callbacks. Best to run this function on Game State's tick
-	UFUNCTION(BlueprintCallable, Category = "C++ Function Library|Online|Steam", meta = (DisplayName = "Run Callbacks", Keywords = "run check refresh tick callbacks steam sdk"))
+	UFUNCTION(BlueprintCallable, Category = "Steam", meta = (DisplayName = "Run Callbacks", Keywords = "run check refresh tick callbacks steam sdk"))
 		static void RunCallbacks();
+
+
+	/* ------------------------------------------------------------ ISteamApps ------------------------------------------------------------ */
+
+	// Gets all registered DLCs and returns their metadata
+	UFUNCTION(BlueprintCallable, Category = "Steam|Steam Apps", meta = (DisplayName = "Get DLC Data", Keywords = "run check refresh tick callbacks steam sdk"))
+		static bool GetAllDLCs(TArray<FSteamDLC>& allDLCs);
+
+	/*Checks if a specific app is installed.
+Does NOT verify ownership
+Does not work for DLCs*/
+	UFUNCTION(BlueprintCallable, Category = "Steam|Steam Apps", meta = (DisplayName = "Get DLC Data", Keywords = "run check refresh tick callbacks steam sdk"))
+		static bool isAppInstalled(int steamAppID);
 	
 };
