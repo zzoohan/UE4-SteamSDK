b69d2989ce022a215ace4898d442dee624dd725f
Cyrus Cook <11858105+cyruscook@users.noreply.github.com>
2019-02-18 19:40:49 +0000

    Added isLowViolence, getBuildId and getAppInstallDir

--- MyBlueprintFunctionLibrary.cpp
+++ MyBlueprintFunctionLibrary.cpp
@@ -5,56 +5,83 @@
 // Include Steam SDK
 #include "ThirdParty/Steamworks/Steamv139/sdk/public/steam/steam_api.h"
 
-bool UMyBlueprintFunctionLibrary::IsSteamAPIRunning() {
+bool UMyBlueprintFunctionLibrary::IsSteamAPIRunning()
+{
 	return SteamAPI_Init();
 }
 
-void UMyBlueprintFunctionLibrary::RestartAppIfNecessary(const int32 SteamAppID = 480) {
+void UMyBlueprintFunctionLibrary::RestartAppIfNecessary(const int32 SteamAppID = 480)
+{
 	// Since we can only run a game through steam on a shipping build, this function should do nothing if it isn't a shipping build
 	#if UE_BUILD_SHIPPING
-	if (SteamAPI_RestartAppIfNecessary(SteamAppID)) {
+	if (SteamAPI_RestartAppIfNecessary(SteamAppID))
+	{
 		FGenericPlatformMisc::RequestExit(true);
 	}
 	#endif
 }
 
-void UMyBlueprintFunctionLibrary::RunCallbacks() {
+void UMyBlueprintFunctionLibrary::RunCallbacks()
+{
 	SteamAPI_RunCallbacks();
 }
 
 /* ------------------------------------------------------------ ISteamApps ------------------------------------------------------------ */
 
-bool UMyBlueprintFunctionLibrary::GetAllDLCs(TArray<FSteamDLC>& allDLCs) {
+bool UMyBlueprintFunctionLibrary::GetAllDLCs(TArray<FSteamDLC>& allDLCs)
+{
 	bool wasSuccessful = true;
 	// Get the total number of DLCs and iterate through them
 	int32 DLCCount = SteamApps()->GetDLCCount();
-	for (int i = 0; i < DLCCount; ++i) {
+	for (int i = 0; i < DLCCount; ++i)
+	{
 		// Request DLC at index i
 		AppId_t appId;
 		bool available;
 		char name[128];
-		bool success = SteamApps()->BGetDLCDataByIndex(i, &appId, &available, name, 128);
-		// If it was successful add it to the array to be returned, otherwise make sure we tell the user it was unsuccessful
-		if (success) {
+
+		// Run Query. If it was successful add it to the array to be returned, otherwise make sure we tell the user it was unsuccessful
+		if (SteamApps()->BGetDLCDataByIndex(i, &appId, &available, name, 128))
+		{
 			allDLCs[i].DLCIndex = i;
 			allDLCs[i].steamAppID = appId;
 			allDLCs[i].isAvailable = available;
 			allDLCs[i].DLCName = name;
-		} else {
+		} else
+		{
 			wasSuccessful = false;
 		}
 	}
 	return wasSuccessful;
 }
 
-bool UMyBlueprintFunctionLibrary::isAppInstalled(int steamAppID = 480) {
+bool UMyBlueprintFunctionLibrary::isAppInstalled(int steamAppID = 480)
+{
 	return SteamApps()->BIsAppInstalled(steamAppID);
 }
 
-bool UMyBlueprintFunctionLibrary::isDlcInstalled(int steamAppID) {
+bool UMyBlueprintFunctionLibrary::isDlcInstalled(int steamAppID)
+{
 	return SteamApps()->BIsDlcInstalled(steamAppID);
 }
 
 bool UMyBlueprintFunctionLibrary::isCybercafe() {
 	return SteamApps()->BIsCybercafe();
+}
+
+bool UMyBlueprintFunctionLibrary::isLowViolence()
+{
+	return SteamApps()->BIsLowViolence();
+}
+
+int UMyBlueprintFunctionLibrary::getBuildId()
+{
+	return SteamApps()->GetAppBuildId();
+}
+
+FString UMyBlueprintFunctionLibrary::getAppInstallDir(int32 steamAppId)
+{
+	char directory[128];
+	//SteamApps()->GetAppInstallDir(steamAppId, directory, 128);
+	return directory;
 }
--- MyBlueprintFunctionLibrary.h
+++ MyBlueprintFunctionLibrary.h
@@ -67,11 +67,27 @@ Does not work for DLCs*/
 	UFUNCTION(BlueprintPure, Category = "Steam|Steam Apps", meta = (DisplayName = "Is App Installed", Keywords = "is app program game installed downloaded steam sdk"))
 		static bool isAppInstalled(int steamAppID);
 
+	/*Checks if the user owns a specific DLC and if the DLC is installed
+Does NOT verify ownership
+Does not work for DLCs*/
 	UFUNCTION(BlueprintPure, Category = "Steam|Steam Apps", meta = (DisplayName = "Is DLC Installed", Keywords = "is DLC installed downloaded steam sdk"))
 		static bool isDlcInstalled(int steamAppID);
 
 	// Checks whether the current App ID is for Cyber Cafes.
 	UFUNCTION(BlueprintPure, Category = "Steam|Steam Apps", meta = (DisplayName = "Is Cybercafe", Keywords = "is in at cybercafe license app id steam sdk"))
 		static bool isCybercafe();
+
+	// Checks if the license owned by the user provides low violence depots.
+	UFUNCTION(BlueprintPure, Category = "Steam|Steam Apps", meta = (DisplayName = "Is Low Violence", Keywords = "is low violence license depot steam sdk"))
+		static bool isLowViolence();
+
+	/* Gets the buildid of this app, may change at any time based on backend updates to the game.
+Defaults to 0 if you're not running a build downloaded from steam.*/
+	UFUNCTION(BlueprintPure, Category = "Steam|Steam Apps", meta = (DisplayName = "Get Build Id", Keywords = "get build id steam sdk"))
+		static int getBuildId();
 	
+	/* Gets the install folder for a specific AppID.
+This works even if the application is not installed, based on where the game would be installed with the default Steam library location.*/
+	UFUNCTION(BlueprintPure, Category = "Steam|Steam Apps", meta = (DisplayName = "Get App Install Dir", Keywords = "get install directory file location game program app steam sdk"))
+		static FString getAppInstallDir(int32 steamAppId);
 };
